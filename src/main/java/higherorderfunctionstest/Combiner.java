2c47215a515f084db66ac08134da78e0fab99de7
Pulvr <pulv3rtoastmann@gmail.com>
2024-11-30 17:27:36 +0100

    PlaystoreMock Selfmade

---
+++ src/main/java/higherorderfunctionstest/Combiner.java
@@ -0,0 +1,12 @@
+package higherorderfunctionstest;
+
+/**
+ * Kombiniert zwei werte und gibt sie in einem gewissen Rückgabewert typen aus
+ *
+ * @param <T> Werte die kombiniert werden sollen
+ * @param <U> Rückgabewert
+ */
+@FunctionalInterface
+public interface Combiner<T, U> {
+    U combine(T element1, T element2);
+}
