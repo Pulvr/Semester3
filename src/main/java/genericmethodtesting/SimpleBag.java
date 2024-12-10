be5038214da1e58740580c69c0568d078e4c7c98
Pulvr <pulv3rtoastmann@gmail.com>
2024-12-10 19:46:25 +0100

    myList is done added SimpleBag.java from generics testing

---
+++ src/main/java/genericmethodtesting/SimpleBag.java
@@ -0,0 +1,33 @@
+package genericmethodtesting;
+
+import java.util.function.Function;
+
+class SimpleBag<E> {
+    private final E element;
+
+    SimpleBag(E value) {
+        this.element = value;
+    }
+
+    E getElement() {
+        return element;
+    }
+
+    public static void main(String[] args) {
+        SimpleBag<Integer> bag = new SimpleBag<>(42);
+
+        //Returns an Integer<E> of SimpleBag as Integer, which is here
+        //autoboxed to int.
+        int elementsAsInt = bag.getElement();
+        System.out.println(elementsAsInt);
+
+        //R: DOUBLE                      E:Int             R: Double
+        double elementAsDouble = bag.map(element -> Double.valueOf(element));
+        System.out.println(elementAsDouble);
+    }
+
+    <R> R map(Function<E, R> mapper) { //verwendet Functional Interface FUNCTION
+        return mapper.apply(element);
+    }
+
+}
--- src/main/java/higherorderfunctionstest/MyList.java
+++ src/main/java/higherorderfunctionstest/MyList.java
@@ -1,17 +1,32 @@
 package higherorderfunctionstest;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
 
 public class MyList<T> {
+    private List<T> elements = new ArrayList<>();
 
     public MyList(List<T> list) {
+        this.elements = list;
     }
 
     public boolean contains(String myString) {
-        Predicate<String> predicate = (stringContent) -> stringContent.contains(myString);
-        
+        Predicate<T> predicate = element -> element.equals(myString);
+        for (T element : elements) {
+            if (predicate.test(element)) {
+                return true;
+            }
+        }
         return false;
+    }
 
+    public boolean contains(Predicate<T> predicate) {
+        for (T element : elements) {
+            if (predicate.test(element)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
