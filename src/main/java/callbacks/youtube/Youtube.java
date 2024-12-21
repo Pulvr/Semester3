d0f2c4ebb31d4c9ade3c22e72b678f105c9a841a
Pulvr <pulv3rtoastmann@gmail.com>
2024-12-21 10:46:50 +0100

    youtube klassen für callback aufgabe

--- src/main/java/callbacks/youtube/Youtube.java
+++ src/main/java/callbacks/youtube/Youtube.java
@@ -17,6 +17,13 @@ class Youtube {
         chris.add(new Video("I dont wanna do this anymore"));
         linus.add(new Video("Uni ist zu hart für dich"));
         anna.add((new Video("Zockung 2024")));
+
+        //Goaloutput
+        /* Sara has been notified of a new video 'I dont wanna do this anymore' by Chris
+        Linus has been notified of a new video 'I dont wanna do this anymore' by Chris
+        Chris has been notified of a new video 'Uni ist zu hart für dich' by Linus
+        Chris has been notified of a new video 'Zockung 2024' by Anna
+        Linus has been notified of a new video 'Zockung 2024' by Anna */
     }
 
 }
